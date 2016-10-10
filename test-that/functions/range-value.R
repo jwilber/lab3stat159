86a8849fd48c3b3f6ae246dea89f230eefcfdbfd
Jared Wilber <j.wilber92@gmail.com>
2016-10-10 12:54:45 -0700

    added na.rm=T arguemtn to range-value.R

--- test-that/functions/range-value.R
+++ test-that/functions/range-value.R
@@ -1,3 +1,7 @@
-range_value <- function(x) {
-  max(x) - min(x)
+range_value <- function(x, na.rm=T) {
+  if (na.rm == TRUE) {
+    return( max(x, na.rm=T) - min(x,na.rm=T))
+  } else {
+    return (max(x) - min(x))
+  }
 }
