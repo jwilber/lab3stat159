1f9c6eaf47335091fe48a89eb6f391f01fa0480e
Jared Wilber <j.wilber92@gmail.com>
2016-10-10 12:48:38 -0700

    added files from lab7

---
+++ test-that/tests/tests.R
@@ -0,0 +1,45 @@
+library(testthat)
+x <- c(1, 2, 3, 4, 5)
+y <- c(1, 2, 3, 4, NA)
+z <- c(TRUE, FALSE, TRUE)
+w <- letters[1:5]
+
+
+# load the source code of the functions to be tested
+source("~/Desktop/stat159/lab3stat159/test-that/functions")
+# context with one test that groups expectations
+context("Test for range value") 
+test_that("range works as expected", {
+  x <- c(1, 2, 3, 4, 5)
+  
+  expect_equal(range_value(x), 4)
+  expect_length(range_value(x), 1)
+  expect_type(range_value(x), 'double')
+})
+
+test_file("~/Desktop/stat159/lab3stat159/test-that/tests/tests.R")
+
+# Your Turn
+test_that("range works as expected for y", {
+  y <- c(1, 2, 3, 4, NA)
+  
+  expect_equal(range_value(y), NA)
+  expect_length(range_value(y), 1)
+  expect_type(range_value(y), 'NA_real_')
+})
+
+
+test_that("range works as expected for z", {
+  z <- c(TRUE, FALSE, TRUE)
+  
+  expect_equal(range_value(z), 1L)
+  expect_length(range_value(z), 1)
+  expect_type(range_value(z), 'integer')
+})
+
+
+test_that("range works as expected for w", {
+  w <- letters[1:5]
+  
+  expect_error(range_value(w), w)
+})
